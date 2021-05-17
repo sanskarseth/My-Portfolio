143296a8dc0e7d63b91f33df918a559fc1807db0
Sanskar <sethsanskar@gmail.com>
2021-05-17 12:19:59 +0530

    updated links

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,25 +1,26 @@
 export const projectList = [
 	{
-		title: 'ShoppeR',
-		about:
-			'This is an e-commerce platform(website) for different clubs of the college to sell their clothings made with MERN stack.',
-		link: 'https://dshopper.netlify.app/',
-		color: '#eaa221',
-		client: 'https://github.com/sanskarseth/ShoppeR',
-		server: 'https://github.com/sanskarseth/ShoppeR-server',
-	},
-	{
-		title: 'Getit',
+		title: 'GetiT',
 		about:
 			'This is a mobile application for selling products around a local area like inside a college campus made with React Native i.e. MERN stack.',
-		link: 'https://github.com/sanskarseth/Getiiit',
+		link: 'https://drive.google.com/drive/folders/1WLbxC3IMPv5u_vmNvY2rL7MRppClI8gi?usp=sharing',
 		color: '#ff6680',
-		client: 'https://github.com/sanskarseth/Getiiit',
+		client: 'https://github.com/sanskarseth/GetiT-Client',
+		server: 'https://github.com/sanskarseth/GetiT-Server',
+	},
+	{
+		title: 'ShoppeR',
+		about:
+			'ShoppeR is an e-commerce website for selling items in the college which is made with MERN Stack.',
+		link: 'https://www.iiitrshops.tech/',
+		color: '#eaa221',
+		client: 'https://github.com/sanskarseth/ShoppeR',
+		server: 'https://github.com/sanskarseth/ShoppeR-Server',
 	},
 	{
 		title: 'CricPing',
 		about:
-			'This is a python script which remind users about the upcoming cricket matches through desktop notifications.',
+			'This is a python script that reminds users about the upcoming cricket matches through toss details by sending desktop notifications.',
 		link: 'https://github.com/sanskarseth/CricPing',
 		color: '#53DC57',
 		client: 'https://github.com/sanskarseth/CricPing',
@@ -28,7 +29,7 @@ export const projectList = [
 		title: 'Portfolio',
 		about:
 			'This is my porfolio website describing about myself made with React.',
-		link: 'https://sanskarseth.github.io/My-Portfolio/',
+		link: '',
 		color: '#5C73F2',
 		client: 'https://github.com/sanskarseth/sanskarseth.github.io',
 	},
